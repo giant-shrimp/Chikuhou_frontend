f1b087df0b3af9441386087e05bcb58f2b146f74
giant-shrimp <yaozaiqiantian4@gmail.com>
2024-10-11 16:07:25 +0900

    サブ機能画面の修正

--- lib/src/screens/drag_drop.dart
+++ lib/src/screens/drag_drop.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DragDropScreen extends StatelessWidget {
-  const DragDropScreen({Key? key}) : super(key: key);
+  const DragDropScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,54 +10,88 @@ class DragDropScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.sub_extension),
       ),
-      body: DragDropContainer(),
+      body: Stack(
+        children: [
+          const DragDropContainer(), // ドラッグドロップエリア
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: BottomNavigationBar(
+              currentIndex: 2, // デフォルトでホームを選択
+              onTap: null, // タップを無効化
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    icon: const Icon(Icons.table_rows_rounded),
+                    label: AppLocalizations.of(context)!.menu
+                ),
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.cloudy_snowing, color: Colors.redAccent),
+                  label: '1st',
+                ),
+                BottomNavigationBarItem(
+                    icon: const Icon(Icons.home),
+                    label: AppLocalizations.of(context)!.home
+                ),
+                const BottomNavigationBarItem(
+                    icon: Icon(Icons.volume_up, color: Colors.redAccent),
+                    label: '2nd'
+                ),
+                BottomNavigationBarItem(
+                    icon: const Icon(Icons.directions_walk_sharp),
+                    label: AppLocalizations.of(context)!.status
+                ),
+              ],
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: Colors.grey, // 選択中のアイテムの色
+              unselectedItemColor: Colors.grey, // 未選択のアイテムの色
+            ),
+          ),
+          // 上部に空箱を追加
+          Positioned(
+            top: 590,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //const EmptyBox(),
+                EmptyBox1(
+                  icon: Icons.cloudy_snowing,
+                  label: AppLocalizations.of(context)!.rain_cloud_radar,
+                ),
+                EmptyBox2(
+                  icon: Icons.volume_up,
+                  label: AppLocalizations.of(context)!.audio_guidance,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
 class DragDropContainer extends StatefulWidget {
+  const DragDropContainer({super.key});
+
   @override
   _DragDropContainerState createState() => _DragDropContainerState();
 }
 
 class _DragDropContainerState extends State<DragDropContainer> {
-  // Combine icon and label together
   List<Map<String, dynamic>> items = [];
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-
-    // Initialize items with localized labels
     items = [
-      {
-        'icon': Icons.cloudy_snowing,
-        'label': AppLocalizations.of(context)!.rain_cloud_radar,
-      },
-      {
-        'icon': Icons.volume_up,
-        'label': AppLocalizations.of(context)!.audio_guidance,
-      },
-      {
-        'icon': Icons.g_translate,
-        'label': AppLocalizations.of(context)!.simple_translation,
-      },
-      {
-        'icon': Icons.forum,
-        'label': AppLocalizations.of(context)!.reviews,
-      },
-      {
-        'icon': Icons.directions_run,
-        'label': AppLocalizations.of(context)!.marathon_course,
-      },
-      {
-        'icon': Icons.add_home_work,
-        'label': AppLocalizations.of(context)!.securing_evacuation_routes,
-      },
-      {
-        'icon': Icons.monitor_weight,
-        'label': AppLocalizations.of(context)!.calorie_count,
-      },
+      {'icon': Icons.cloudy_snowing, 'label': AppLocalizations.of(context)!.rain_cloud_radar},
+      {'icon': Icons.volume_up, 'label': AppLocalizations.of(context)!.audio_guidance},
+      {'icon': Icons.g_translate, 'label': AppLocalizations.of(context)!.simple_translation},
+      {'icon': Icons.forum, 'label': AppLocalizations.of(context)!.reviews},
+      {'icon': Icons.directions_run, 'label': AppLocalizations.of(context)!.marathon_course},
+      {'icon': Icons.add_home_work, 'label': AppLocalizations.of(context)!.securing_evacuation_routes},
+      {'icon': Icons.monitor_weight, 'label': AppLocalizations.of(context)!.calorie_count},
     ];
   }
 
@@ -112,7 +146,7 @@ class DragItem extends StatelessWidget {
   final IconData icon;
   final String label;
 
-  const DragItem({required this.icon, required this.label});
+  const DragItem({super.key, required this.icon, required this.label});
 
   @override
   Widget build(BuildContext context) {
@@ -130,6 +164,101 @@ class DragItem extends StatelessWidget {
           Text(
             label,
             style: const TextStyle(color: Colors.white),
+            textAlign: TextAlign.center, // テキストを中央揃え
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// 空の箱ウィジェット
+class EmptyBox extends StatelessWidget {
+  const EmptyBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 125,
+      height: 125,
+      decoration: BoxDecoration(
+        color: Colors.grey[400], // 背景色を設定
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: const Center(
+        child: Text(
+          '右のボックスに\nサブ機能を入れて\nカスタマイズする\nことができます',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
+
+// アイコンとラベルを持つ空箱ウィジェット
+class EmptyBox1 extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const EmptyBox1({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 125,
+      height: 125,
+      decoration: BoxDecoration(
+        color: Colors.redAccent, // 背景色を設定
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: Colors.white, size: 50),
+          const SizedBox(height: 10),
+          Text(
+            label,
+            style: const TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EmptyBox2 extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const EmptyBox2({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 125,
+      height: 125,
+      decoration: BoxDecoration(
+        color: Colors.redAccent, // 背景色を設定
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: Colors.white, size: 50),
+          const SizedBox(height: 10),
+          Text(
+            label,
+            style: const TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
           ),
         ],
       ),
