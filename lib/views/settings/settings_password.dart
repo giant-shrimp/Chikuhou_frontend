e03c87885a5aee530547590f37d9a3e22174022a
giant-shrimp <yaozaiqiantian4@gmail.com>
2024-11-25 00:36:59 +0900

    パスワード変更の分離

--- lib/views/settings/settings_password.dart
+++ lib/views/settings/settings_password.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../../config/providers/viewmodel_provider.dart';
+import '../../core/utils/dialog_helpers.dart';
+import '../widgets/common/custom_button.dart';
+import '../widgets/common/custom_text_field.dart';
 
-class SettingsPassword extends StatelessWidget {
+class SettingsPassword extends ConsumerWidget {
   const SettingsPassword({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModel = ref.watch(passwordSettingsViewModelProvider);
+    final viewModelNotifier = ref.read(passwordSettingsViewModelProvider.notifier);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.password),
@@ -15,39 +23,47 @@ class SettingsPassword extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              obscureText: true, // パスワード入力を非表示
-              decoration: InputDecoration(
-                labelText: AppLocalizations.of(context)!.current_password,
-                prefixIcon: const Icon(Icons.lock_outline),
-              ),
+            CustomTextField(
+              label: AppLocalizations.of(context)!.current_password,
+              icon: Icons.lock_outline,
+              isPassword: true,
+              onChanged: viewModelNotifier.updateCurrentPassword,
             ),
             const SizedBox(height: 16),
-            TextField(
-              obscureText: true, // パスワード入力を非表示
-              decoration: InputDecoration(
-                labelText: AppLocalizations.of(context)!.new_password,
-                prefixIcon: const Icon(Icons.lock),
-              ),
+            CustomTextField(
+              label: AppLocalizations.of(context)!.new_password,
+              icon: Icons.lock,
+              isPassword: true,
+              onChanged: viewModelNotifier.updateNewPassword,
             ),
             const SizedBox(height: 16),
-            TextField(
-              obscureText: true, // パスワード確認を非表示
-              decoration: InputDecoration(
-                labelText: AppLocalizations.of(context)!.new_password_confirmation,
-                prefixIcon: const Icon(Icons.lock),
-              ),
+            CustomTextField(
+              label: AppLocalizations.of(context)!.new_password_confirmation,
+              icon: Icons.lock,
+              isPassword: true,
+              onChanged: viewModelNotifier.updateConfirmPassword,
             ),
             const SizedBox(height: 60),
-            ElevatedButton(
-              onPressed: () {
+            CustomButton(
+              text: AppLocalizations.of(context)!.change_password,
+              onPressed: () async {
+                final success = await viewModelNotifier.changePassword();
+                if (success) {
+                  // 成功した場合、ダイアログなどで通知を表示
+                  await showDialogs(
+                    context: context,
+                    title: AppLocalizations.of(context)!.change_password_success,
+                    confirmText: AppLocalizations.of(context)!.ok,
+                  );
+                } else {
+                  // 失敗した場合、エラーダイアログを表示
+                  await showDialogs(
+                    context: context,
+                    title: AppLocalizations.of(context)!.change_password_error,
+                    confirmText: AppLocalizations.of(context)!.ok,
+                  );
+                }
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white, // 背景色を白に設定
-                foregroundColor: Colors.black, // テキスト色を黒に設定
-                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 50),
-              ),
-              child: Text(AppLocalizations.of(context)!.change_password),
             ),
           ],
         ),
