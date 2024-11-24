1fb5516454efa93e74e05a0dc6192eecc9e36021
giant-shrimp <yaozaiqiantian4@gmail.com>
2024-11-25 00:24:40 +0900

    ダイアログの汎用性向上

--- lib/core/utils/dialog_helpers.dart
+++ lib/core/utils/dialog_helpers.dart
@@ -1,36 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-/// ログアウト確認ダイアログを表示するヘルパー関数
+/// 汎用ダイアログを表示するヘルパー関数
 Future<void> showDialogs({
   required BuildContext context,
   required String title,
-  required String content,
-  required String cancelText,
-  required String confirmText,
-  required VoidCallback onConfirm,
+  String? content,
+  String? cancelText, // キャンセルボタンのテキスト（オプショナル）
+  String? confirmText, // 確認ボタンのテキスト（オプショナル）
+  VoidCallback? onConfirm, // 確認ボタンの処理（オプショナル）
 }) async {
   showDialog(
     context: context,
     builder: (BuildContext context) {
       return CupertinoAlertDialog(
         title: Text(title),
-        content: Text(content),
+        content: content != null
+            ? Text(content)
+            : const SizedBox.shrink(), // contentがnullの場合は空のウィジェット
         actions: <Widget>[
-          CupertinoDialogAction(
-            child: Text(cancelText),
-            onPressed: () {
-              Navigator.of(context).pop(); // ダイアログを閉じる
-            },
-          ),
-          CupertinoDialogAction(
-            isDestructiveAction: true, // 強調表示
-            child: Text(confirmText),
-            onPressed: () {
-              Navigator.of(context).pop(); // ダイアログを閉じる
-              onConfirm(); // 確認ボタンの処理を実行
-            },
-          ),
+          // キャンセルボタンが必要な場合のみ表示
+          if (cancelText != null)
+            CupertinoDialogAction(
+              child: Text(cancelText),
+              onPressed: () {
+                Navigator.of(context).pop(); // ダイアログを閉じる
+              },
+            ),
+          // 確認ボタンが必要な場合のみ表示
+          if (confirmText != null)
+            CupertinoDialogAction(
+              isDestructiveAction: true, // 強調表示
+              child: Text(confirmText),
+              onPressed: () {
+                Navigator.of(context).pop(); // ダイアログを閉じる
+                if (onConfirm != null) {
+                  onConfirm(); // 確認ボタンの処理を実行
+                }
+              },
+            ),
         ],
       );
     },
