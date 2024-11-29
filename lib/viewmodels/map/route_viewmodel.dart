0089e1b19845c18896ef4df7179c1b5c9e40bb5e
feynman <feynman.diagram.511@gmail.com>
2024-11-29 23:49:00 +0900

    Move the view to the starting point

--- lib/viewmodels/map/route_viewmodel.dart
+++ lib/viewmodels/map/route_viewmodel.dart
@@ -124,6 +124,24 @@ class RouteViewModel extends ChangeNotifier {
     return leastGradientRoute;
   }
 
+  Future<LatLng> fetchCoordinatesFromAddress(String address) async {
+    final url =
+        'https://maps.googleapis.com/maps/api/geocode/json?address=$address&key=$apiKey';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      if (data['results'] != null && data['results'].isNotEmpty) {
+        final location = data['results'][0]['geometry']['location'];
+        return LatLng(location['lat'], location['lng']);
+      } else {
+        throw Exception('住所の座標が見つかりませんでした');
+      }
+    } else {
+      throw Exception('住所の座標取得エラー: ${response.statusCode}');
+    }
+  }
+
   Future<List<double>> fetchElevationsForPolyline(
       List<LatLng> polylinePoints) async {
     const double segmentDistance = 50.0;
